e6b86a125db74d9e78f206ee7ce795f787a638bd
lvjun <lvjun2@yusys.com.cn>
2018-11-30 14:21:54 +0800

    注解@Select

--- src/com/xfwl/imapper/teachtasks/TeachTaskMapper.java
+++ src/com/xfwl/imapper/teachtasks/TeachTaskMapper.java
@@ -2,6 +2,8 @@ package com.xfwl.imapper.teachtasks;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Select;
+
 import com.xfwl.pojo.Card;
 import com.xfwl.pojo.TCourseTasks;
 import com.xfwl.pojo.Teacher;
@@ -14,6 +16,13 @@ public interface TeachTaskMapper {
 	 * @return
 	 */
 	public Teacher qryTeacherById(int t_id);
+	/**
+	 * 根据教师ID查询教师信息（使用注解方式）：不需要在mapper映射文件中配置了
+	 * @param t_id
+	 * @return
+	 */
+	@Select("select t_id,t_name,t_sex,t_address from teacher where t_id=#{t_id}")
+	public Teacher qryTeacherByIdOnAnnotations(int t_id);
 	/**
 	 * 根据教师ID查询教师登机牌信息
 	 * @param t_id
